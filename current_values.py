1a092be55dc3367631d74ce745354445d47f4e1e
Andre Müller <gbs.deadeye@gmail.com>
2020-02-13 15:55:28 +0100

    Bugfix

--- current_values.py
+++ current_values.py
@@ -4,26 +4,26 @@ import struct
 FD = None
 MM = None
 
+
 def _get_values():
-        """
+    """
         Format:
         5i: id, row, cycle, capacity, error
         9f: voltage, current, charge, temperature, timestamp, 4 x cell_voltages
         """
-        global FD, MM
-        st = struct.Struct('<5i9f')
-        if FD is None:
-            fd = open('/media/data/current_values.bin', 'rb')
-        if MM is None:
-            MM = mmap.mmap(fd.fileno(), st.size, prot=mmap.PROT_READ)
-        return st.unpack_from(MM, 0)
-
+    global FD, MM
+    st = struct.Struct('<5i9f')
+    if FD is None:
+        FD = open('/media/data/current_values.bin', 'rb')
+    if MM is None:
+        MM = mmap.mmap(FD.fileno(), st.size, access=mmap.ACCESS_READ)
+    return st.unpack_from(MM, 0)
 
 
 def get_values():
     try:
         values = _get_values()
-    except:
+    except ValueError:
         return {}
     topics = ('id', 'row', 'cycle', 'capacity', 'error', 'voltage', 'current', 'charge', 'temperature', 'timestamp')
     data = dict(zip(topics, values))
