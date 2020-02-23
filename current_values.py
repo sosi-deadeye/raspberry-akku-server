47e2afe2f8d8dc0250f1a8381fec362823d2c7c7
Andre Müller <gbs.deadeye@gmail.com>
2020-02-23 18:46:25 +0100

    added MemoryMappedStruct and prepared for use in server.py

--- current_values.py
+++ current_values.py
@@ -1,32 +1,96 @@
 import mmap
 import struct
+from pathlib import Path
+from typing import Any, Iterable, Union, Tuple
 
-FD = None
-MM = None
+
+class MemoryMappedStruct:
+    def __init__(
+        self,
+        file: Union[Path, str],
+        struct: struct.Struct,
+        *,
+        reader: bool = False,
+        writer: bool = False,
+        create: bool = False,
+    ):
+        self.file = Path(file)
+        self.mm_st = struct
+        self.create_file = create
+        self.flush_mm = writer
+        if reader and not writer:
+            self.mm = self._get_mmap_reader()
+        elif not reader and writer:
+            self.mm = self._get_mmap_writer()
+        elif not reader and not writer:
+            raise ValueError("Must be a reader or a writer.")
+        else:
+            raise ValueError("Can't be a reader and writer.")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def _get_mmap_reader(self) -> mmap.mmap:
+        with self.file.open("rb") as fd:
+            return mmap.mmap(fd.fileno(), self.mm_st.size, access=mmap.ACCESS_READ)
+
+    def _get_mmap_writer(self) -> mmap.mmap:
+        if self.create_file:
+            with self.file.open("wb") as fd:
+                fd.write(b"\x00" * self.mm_st.size)
+        with self.file.open("r+b") as fd:
+            return mmap.mmap(
+                fileno=fd.fileno(), length=self.mm_st.size, access=mmap.ACCESS_WRITE,
+            )
+
+    def close(self) -> None:
+        if not self.mm.closed:
+            if self.flush_mm:
+                self.mm.flush()
+        self.mm.close()
+
+    def get_values(self) -> Tuple[Any]:
+        return self.mm_st.unpack_from(self.mm, 0)
+
+    def set_values(self, values: Iterable[Any]):
+        self.mm_st.pack_into(self.mm, 0, *values)
 
 
-def _get_values():
-    """
-        Format:
-        5i: id, row, cycle, capacity, error
-        9f: voltage, current, charge, temperature, timestamp, 4 x cell_voltages
-        """
-    global FD, MM
-    st = struct.Struct('<5i9f')
-    if FD is None:
-        FD = open('/media/data/current_values.bin', 'rb')
-    if MM is None:
-        MM = mmap.mmap(FD.fileno(), st.size, access=mmap.ACCESS_READ)
-    return st.unpack_from(MM, 0)
+def _get_values(mmap_reader: MemoryMappedStruct, topics: Tuple[str, ...]):
+    try:
+        values = mmap_reader.get_values()
+    except ValueError:
+        return {}
+    data = dict(zip(topics, values))
+    cell_voltages = values[-4:]
+    data["cell_voltages"] = cell_voltages
+    return data
 
 
 def get_values():
-    try:
-        values = _get_values()
-    except ValueError:
-        return {}
-    topics = ('id', 'row', 'cycle', 'capacity', 'error', 'voltage', 'current', 'charge', 'temperature', 'timestamp')
-    data = dict(zip(topics, values))
-    cell_voltages = values[-4:]
-    data['cell_voltages'] = cell_voltages
-    return data
+    return _get_values(MM_READER, TOPICS)
+
+
+def set_values(values):
+    return MM_WRITER.set_values(values)
+
+
+FILE = "/media/data/current_values.bin"
+STRUCT = struct.Struct("<5i9f")
+TOPICS = (
+    "id",
+    "row",
+    "cycle",
+    "capacity",
+    "error",
+    "voltage",
+    "current",
+    "charge",
+    "temperature",
+    "timestamp",
+)
+MM_READER = MemoryMappedStruct(FILE, STRUCT, reader=True)
+MM_WRITER = MemoryMappedStruct(FILE, STRUCT, writer=True, create=False)
